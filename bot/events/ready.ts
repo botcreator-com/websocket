0d68ab821f745c16ef97ed8656a5ff877bf1789f
garder500 <jeremy27.clara22@gmail.com>
2022-01-06 11:13:48 +0100

    Updates

--- bot/events/ready.ts
+++ bot/events/ready.ts
@@ -1,10 +1,9 @@
 
 import {blue, green} from "colors";
 import fetch from "node-fetch";
-import WebSocket from "ws";
-import {workerData} from "worker_threads";
 import {Collection, Guild, Webhook} from "discord.js";
-export = async (client: any) => {
+import ExtendedClient from "../extendedClient";
+export = async (client: ExtendedClient) => {
 
     console.log(`Logged in as ${blue(`${client?.user?.tag}`)}`);
     await client?.user?.setStatus("idle");
@@ -41,77 +40,7 @@ export = async (client: any) => {
         }
 
     });
-    client.functions.EOR = function EOR (e: any, message: any) {
 
-        const embed = {
-            "author": {
-                "name": message.author
-                    ? message.author.tag
-                    : message.user.tag,
-                "icon_url": message.author
-                    ? message.author.avatarURL()
-                    : message.user.avatarURL()
-            },
-            "color": "#5e5afc",
-            "timestamp": new Date(),
-            "footer": {
-                "text": "Global Bot",
-                "icon_url": client?.user?.avatarURL()
-            },
-            "fields": [],
-            "title": null,
-            "description": ""
-        };
-        if (e.error) {
-
-            if (e.error === "yes") {
-
-                embed.color = "0xf00020";
-
-            } else if (e.error === "no") {
-
-                embed.color = "0x008000";
-
-            }
-
-        }
-        if (e.fields) {
-
-            embed.fields = e.fields;
-
-        }
-        if (e.title) {
-
-            embed.title = e.title;
-
-        }
-        if (e.desc) {
-
-            embed.description = e.desc;
-
-        }
-        return embed;
-
-    };
-
-    client.functions.getLastDay = function getLastDay (year: number, month: number) {
-
-        return new Date(
-            year,
-            month,
-            0
-        ).getDate();
-
-    };
-
-    client.functions.getDateFromTimestamp = function getDateFromTimestamp (timestamp: number) {
-
-        const date = new Date(timestamp);
-        return `${date.getFullYear()}-${`0${date.getMonth() + 1}`.slice(-2)}-${`0${date.getDate()}`.slice(-2)}`;
-
-    };
-
-    client.myob = new WebSocket(`ws://localhost:${workerData.port}`);
     client.myob.onopen = (): void => {
 
         console.log("Server Open");
@@ -124,47 +53,53 @@ export = async (client: any) => {
 
     };
 
-    client.myob.onmessage = (data: MessageEvent) => {
+    client.myob.onmessage = (data) => {
 
-        const msg = JSON.parse(data.data);
-        if (msg.from !== "bot") {
+        if (typeof data.data === "string") {
 
-            client.guilds.cache.get("919356120466857984").channels.cache.get("919724869376159764").fetchWebhooks().
-                then((webhooks3: Collection<string, Webhook>) => {
+            const msg = JSON.parse(data.data);
+            if (msg.from !== "bot") {
 
-                    const web = webhooks3.filter((e: Webhook) => e.type === "Incoming").first();
-                    if (web) {
+                let channel: any = client?.guilds?.cache.get("919356120466857984")?.channels?.cache?.get("919724869376159764");
+                channel?.fetchWebhooks().
+                    then((webhooks3: Collection<string, Webhook>) => {
 
-                        fetch(
-                            `https://discord.com/api/webhooks/${web.id}/${web.token}`,
-                            {
-                                "method": "post",
-                                "headers": {
-                                    "Content-Type": "application/json"
-                                },
-                                "body": JSON.stringify({
-                                    "username": `${msg.user.username}#${msg.user.discriminator}`,
-                                    "avatar_url": `https://cdn.discordapp.com/avatars/${msg.user.id}/${msg.user.avatar}.png`,
-                                    "content": msg.message
-                                })
-                            }
-                        );
+                        const web = webhooks3.filter((e: Webhook) => e.type === "Incoming").first();
+                        if (web) {
 
-                    } else {
+                            fetch(
+                                `https://discord.com/api/webhooks/${web.id}/${web.token}`,
+                                {
+                                    "method": "post",
+                                    "headers": {
+                                        "Content-Type": "application/json"
+                                    },
+                                    "body": JSON.stringify({
+                                        "username": `${msg.user.username}#${msg.user.discriminator}`,
+                                        "avatar_url": `https://cdn.discordapp.com/avatars/${msg.user.id}/${msg.user.avatar}.png`,
+                                        "content": msg.message
+                                    })
+                                }
+                            );
 
-                        client.guilds.cache.get("919356120466857984").channels.cache.get("919724869376159764").createWebhook(
-                            `${msg.user.username}#${msg.user.discriminator}`,
-                            {
-                                "avatar": `https://cdn.discordapp.com/avatars/${msg.user.id}/${msg.user.avatar}.png`,
-                                "reason": `Création de webhook par : ${msg.user.username}#${msg.user.discriminator}`
-                            }
-                        ).
-                            then((e: Webhook) => e.send(msg.message));
+                        } else {
+
+                            channel = client?.guilds?.cache?.get("919356120466857984")?.channels?.cache?.get("919724869376159764");
+                            channel.createWebhook(
+                                `${msg.user.username}#${msg.user.discriminator}`,
+                                {
+                                    "avatar": `https://cdn.discordapp.com/avatars/${msg.user.id}/${msg.user.avatar}.png`,
+                                    "reason": `Création de webhook par : ${msg.user.username}#${msg.user.discriminator}`
+                                }
+                            ).
+                                then((e: Webhook) => e.send(msg.message));
 
 
-                    }
+                        }
 
-                });
+                    });
+
+            }
 
         }
 
---
+++ bot/extendedClient.ts
@@ -0,0 +1,338 @@
+"strict mode";
+import config from "./config.json";
+import {Client} from "discord.js";
+import WebSocket from "ws";
+import {workerData} from "worker_threads";
+import {readdirSync, createReadStream} from "fs";
+import ms from "ms";
+import {join} from "path";
+import {blue, green, red} from "colors";
+import {text} from "figlet";
+import {
+    NoSubscriberBehavior,
+    StreamType,
+    VoiceConnectionStatus,
+    createAudioPlayer,
+    createAudioResource,
+    entersState,
+    getVoiceConnection,
+    joinVoiceChannel
+} from "@discordjs/voice";
+import {cpus, loadavg, totalmem} from "os";
+const guildInvites = new Map();
+class ExtendedClient extends Client {
+
+    config: any;
+
+    functions: object;
+
+    colors: object;
+
+    guildInvites: Map<string, object | Map<string, object>>;
+
+    footer: string;
+
+    voc: object;
+
+    myob: WebSocket;
+
+    constructor (tok: string) {
+
+        super({"partials": [
+            "USER",
+            "CHANNEL",
+            "GUILD_MEMBER",
+            "MESSAGE",
+            "REACTION"
+        ],
+        "allowedMentions": {
+            "repliedUser": false
+        },
+        "failIfNotExists": false,
+        "messageCacheLifetime": require("ms")("1m"),
+        "intents": [
+            "GUILDS",
+            "GUILD_MEMBERS",
+            "GUILD_BANS",
+            "GUILD_EMOJIS_AND_STICKERS",
+            "GUILD_INTEGRATIONS",
+            "GUILD_INVITES",
+            "GUILD_VOICE_STATES",
+            "GUILD_PRESENCES",
+            "GUILD_MESSAGES",
+            "GUILD_MESSAGE_REACTIONS",
+            "GUILD_MESSAGE_TYPING",
+            "DIRECT_MESSAGES",
+            "DIRECT_MESSAGE_REACTIONS",
+            "DIRECT_MESSAGE_TYPING"
+        ]});
+        this.config = config;
+        this.functions = {
+            "convert": function convert (number: number) {
+
+                const str = String(number),
+                    symbol = Math.floor(str.length / 3.00001),
+                    numberSymbol = [
+                        "",
+                        "K",
+                        "M",
+                        "Md",
+                        "B"
+                    ];
+
+                let num = str.length % 3,
+                    finalNumber = str;
+
+
+                if (symbol > 0) {
+
+                    if (num === 0) {
+
+                        num = 3;
+
+                    }
+                    num += 2;
+                    finalNumber = (Number(str.substr(
+                        0,
+                        num
+                    )) / 100).toFixed(2) + numberSymbol[symbol];
+
+                }
+                return String(finalNumber).replace(
+                    ".00",
+                    ""
+                );
+
+            },
+            "timetrade": function timetrade (time: string | number) {
+
+                let finaltime = 0;
+                if (typeof time === "string") {
+
+                    time.split(" ").forEach((timer) => {
+
+                        if (timer.endsWith("y")) {
+
+                            finaltime += ms(`${Number(timer.replace(
+                                "y",
+                                ""
+                            ))}y`);
+
+                        }
+                        if (timer.endsWith("w")) {
+
+                            finaltime += ms(`${Number(timer.replace(
+                                "w",
+                                ""
+                            ))}w`);
+
+                        }
+                        if (timer.endsWith("d")) {
+
+                            finaltime += ms(`${Number(timer.replace(
+                                "d",
+                                ""
+                            ))}d`);
+
+                        }
+                        if (timer.endsWith("h")) {
+
+                            finaltime += ms(`${Number(timer.replace(
+                                "h",
+                                ""
+                            ))}h`);
+
+                        }
+                        if (timer.endsWith("m")) {
+
+                            finaltime += ms(`${Number(timer.replace(
+                                "m",
+                                ""
+                            ))}m`);
+
+                        }
+                        if (timer.endsWith("s")) {
+
+                            finaltime += ms(`${Number(timer.replace(
+                                "s",
+                                ""
+                            ))}s`);
+
+                        }
+
+                    });
+                    return finaltime;
+
+                }
+                return require("./functions/parsems")(time);
+
+            },
+            "getLastDay": function getLastDay (year: number, month: number) {
+
+                return new Date(
+                    year,
+                    month,
+                    0
+                ).getDate();
+
+            },
+            "getDateFromTimestamp": function getDateFromTimestamp (timestamp: number) {
+
+                const date = new Date(timestamp);
+                return `${date.getFullYear()}-${`0${date.getMonth() + 1}`.slice(-2)}-${`0${date.getDate()}`.slice(-2)}`;
+
+            }
+
+        };
+        this.myob = new WebSocket(`ws://localhost:${workerData.port}`);
+        this.guildInvites = guildInvites;
+        this.colors = {
+            "red": 16711680,
+            "green": 32560,
+            "default": 3092790 // Discord Color
+        };
+        this.voc = {
+            joinVoiceChannel,
+            getVoiceConnection,
+            VoiceConnectionStatus,
+            entersState,
+            createAudioPlayer,
+            NoSubscriberBehavior,
+            StreamType,
+            createAudioResource,
+            createReadStream
+        };
+        this.footer = "MYOB";
+        try {
+
+            this.launch().then(() => {
+
+                console.log(blue("All is launched, Connecting to Discord.."));
+
+            });
+
+        } catch (e: any) {
+
+            throw new Error(e);
+
+        }
+        this.login(tok);
+
+    }
+
+    async launch () {
+
+        console.log(blue("G-Bot Start"));
+        await this._eventsHandler();
+        await this._processEvent();
+        await this._startingMessage();
+
+    }
+
+    public _eventsHandler () {
+
+        let count = 0;
+        const files = readdirSync(join(
+            __dirname,
+            "events"
+        ));
+        files.forEach(async (e) => {
+
+            try {
+
+                count++;
+                const fileName = e.split(".")[0],
+                    file = await import(join(
+                        __dirname,
+                        "events",
+                        e
+                    ));
+                this.on(
+                    fileName,
+                    file.bind(
+                        null,
+                        this
+                    )
+                );
+
+            } catch (error: any) {
+
+                throw new Error(`${red("[Events]")} Failed to load event ${e}: ${error.stack || error}`);
+
+            }
+
+        });
+        console.log(`${green("[Events]")} Loaded ${count}/${files.length} events`);
+
+    }
+
+    public _startingMessage () {
+
+        const cpuCores = cpus().length;
+        // Custom Starting Message
+        text(
+            "Obvision",
+            {
+                "font": "Standard"
+            },
+            (err, data) => {
+
+                if (err) {
+
+                    console.log("Something went wrong...");
+                    console.dir(err);
+                    return;
+
+                }
+                const data2 = data;
+                text(
+                    "A General Bot for Discord",
+                    {
+                    },
+                    (err2, freeData) => {
+
+                        if (err2) {
+
+                            console.log("Something went wrong...");
+                            console.dir(err2);
+                            return;
+
+                        }
+                        console.log(`================================================================================================================================\n${
+                            data2}\n\n${freeData}\n
+                            ================================================================================================================================\n
+                                CPU: ${(loadavg()[0] / cpuCores).toFixed(2)}% / 100%\n
+                                RAM: ${Math.trunc(process.memoryUsage().heapUsed / 1000 / 1000)} MB / ${Math.trunc(totalmem() / 1000 / 1000)} MB
+                                Discord WebSocket Ping: ${this.ws.ping}\n
+                                ================================================================================================================================`);
+
+                    }
+                );
+
+            }
+        );
+
+    }
+
+    public _processEvent () {
+
+        process.on(
+            "unhandledRejection",
+            (error: any) => {
+
+                if (error.code === 50007) {
+
+                    return;
+
+                }
+                console.error(green("✅ An Error has occured : ") + red(error.stack));
+
+            }
+        );
+
+    }
+
+}
+
+export default ExtendedClient;
+
