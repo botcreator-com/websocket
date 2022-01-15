42723a7f063d652a30a3fa2bec64543c3040c31b
Emmanuel MILLIOT <77552003+emmanueldevfr@users.noreply.github.com>
2022-01-15 20:01:38 +0100

    Indentation

--- ws/index.ts
+++ ws/index.ts
@@ -1,77 +1,43 @@
-import WebSocket, {WebSocketServer} from "ws";
-import {createServer} from "http";
-import {workerData} from "worker_threads";
+import WebSocket, { WebSocketServer } from "ws";
+import { createServer } from "http";
+import { workerData } from "worker_threads";
 const server = createServer(),
-    wss = new WebSocketServer({"noServer": true});
+    wss = new WebSocketServer({ "noServer": true });
 
-console.log(
-    "Server started on port : ",
-    workerData.port
-);
+console.log("Server started on port : ", workerData.port);
 
 interface CustomSocket extends WebSocket {
     isAlive: boolean
 }
 
-wss.on(
-    "connection",
-    (ws: CustomSocket, req: any) => {
-
-        console.log(
-            "New connection",
-            req.socket.remoteAddress
+wss.on("connection", (ws: CustomSocket, req: any) => {
+        console.log("New connection", req.socket.remoteAddress
         );
-        ws.on(
-            "pong",
-            () => {
-
+        
+        ws.on( "pong", () => {
                 ws.isAlive = true;
                 return ws;
-
             }
         );
-        ws.on(
-            "message",
-            (data: WebSocketEventMap, isBinary: boolean) => {
 
+        ws.on("message", (data: WebSocketEventMap, isBinary: boolean) => {
                 wss.clients.forEach((client) => {
-
                     if (client.readyState === WebSocket.OPEN) {
-
-                        client.send(
-                            data,
-                            {"binary": isBinary}
+                        client.send(data,{ "binary": isBinary }
                         );
-
                     }
-
                 });
-
             }
         );
-
     }
 );
 
-server.on(
-    "upgrade",
-    (request, socket, head) => {
-
-        wss.handleUpgrade(
-            request,
-            socket,
-            head,
-            () => {
-
-                wss.emit(
-                    "connection",
-                    socket,
-                    request
-                );
-
+server.on("upgrade", (request, socket, head) => {
+        wss.handleUpgrade(request, socket, head,() => {
+                wss.emit("connection",socket, request);
             }
         );
-
     }
 );
+
 server.listen(workerData.port);
