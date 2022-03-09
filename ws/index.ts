ab3fab34e1fa152bcf436c351aaf343c631c8fef
garder500 <jeremy27.clara22@gmail.com>
2022-03-09 23:43:44 +0100

    Minor update

--- ws/index.ts
+++ ws/index.ts
@@ -9,11 +9,12 @@ console.log("Server started on port : ", workerData.port);
 
 wss.on("connection", (ws: WebSocket, req) => {
     console.log("New connection", req.headers["x-forwarded-for"]);
-    ws.on("message", (data, isBinary) => {
+    ws.on("message", (data, isBinary) => { 
+        console.log(data);
         wss.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {
+            if (client.readyState === WebSocket.OPEN && client !== ws) {
                 client.send(data, { binary: isBinary });
-                console.log(data);
+               
             }
         });
     });
