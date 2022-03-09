0872f9584af092753ca8a3ca136b1dc1ea596b14
garder500 <jeremy27.clara22@gmail.com>
2022-03-09 21:02:31 +0100

    Minor fix

--- ws/index.ts
+++ ws/index.ts
@@ -7,21 +7,18 @@ const server = createServer(),
 console.log("Server started on port : ", workerData.port);
 
 
-wss.on("connection", (ws: WebSocket, req) => {
-    ws.emit("message", "Salut, ça va");
-    console.log("New connection", req.headers["x-forwarded-for"]);
-    ws.on("message", (data: string) => {
+wss.on("connection", (ws) => {
+    ws.on("message", (data, isBinary) => {
         console.log(data);
-        ws.emit("message", data);
-        wss.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(data);
-            }
-        });
-    }
-    );
-}
-);
+      wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data, { "binary": isBinary });
+              }
+        }
+      });
+    });
+  });
 
 server.on("upgrade", (request, socket, head) => {
     wss.handleUpgrade(request, socket, head, () => {
