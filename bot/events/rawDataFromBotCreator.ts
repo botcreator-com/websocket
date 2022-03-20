4a16f1f54f82e2024ec3ee32db86caf312aeb169
garder500 <jeremy27.clara22@gmail.com>
2022-03-20 15:28:46 +0100

    Minor "fix"

--- bot/events/rawDataFromBotCreator.ts
+++ bot/events/rawDataFromBotCreator.ts
@@ -1,13 +1,13 @@
 import ExtendedClient from "../extendedClient";
 
 export default async (client: ExtendedClient, data: any) => {
-  try{
+  try {
     const JsonRaw = JSON.parse(String(data));
-     if(JsonRaw.id === client?.user?.id && JsonRaw.event === "stop"){
-      await client?.user?.setActivity(`${client.user?.username} is Stoping...`)
+     if (JsonRaw.id === client?.user?.id && JsonRaw.event === "stop"){
+      await client?.user?.setActivity(`${client.user?.username} is Stoping...`);
     }
-  }catch(e){
-    return;
+  } catch (e){
+    await client?.user?.setActivity("Got an error ...");
   }
   
 };
