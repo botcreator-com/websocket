2d07afee1fe22c140b88e883a5508c3e328d1bb8
garder500 <jeremy27.clara22@gmail.com>
2022-03-20 04:32:55 +0100

    Minor fix on it too

--- ws/index.ts
+++ ws/index.ts
@@ -10,11 +10,9 @@ console.log("Server started on port : ", workerData.port);
 wss.on("connection", (ws: WebSocket, req) => {
     console.log("New connection", req.headers["x-forwarded-for"]);
     ws.on("message", (data, isBinary) => { 
-        console.log(data);
         wss.clients.forEach(function each(client) {
             if (client.readyState === WebSocket.OPEN && client !== ws) {
                 client.send(data, { binary: isBinary });
-               
             }
         });
     });
