54bb17429ccad098b9c9c39041dd48f77de01b57
garder500 <jeremy27.clara22@gmail.com>
2022-03-24 10:34:51 +0100

    Try somes update

--- ws/index.ts
+++ ws/index.ts
@@ -1,14 +1,19 @@
 import WebSocket, { WebSocketServer } from "ws";
 import { createServer } from "http";
 import { workerData } from "worker_threads";
+import { IncomingMessage } from "http";
 const server = createServer(),
     wss = new WebSocketServer({ "noServer": true });
 
 console.log("Server started on port : ", workerData.port);
 
+interface WebSocketAndReq extends WebSocket{
+    req?:IncomingMessage
+}
 
-wss.on("connection", (ws: WebSocket, req) => {
-    console.log("New connection", req.headers["x-forwarded-for"]);
+wss.on("connection", (ws: WebSocketAndReq, req) => {
+    if(!ws.req) ws.req = req;
+    console.log("New connection", ws.req.headers["x-forwarded-for"]);
     ws.on("message", (data, isBinary) => { 
         if(!isBinary){
             wss.clients.forEach(function each(client) {
@@ -22,7 +27,8 @@ wss.on("connection", (ws: WebSocket, req) => {
 );
 
 server.on("upgrade", (request, socket, head) => {
-    wss.handleUpgrade(request, socket, head, function done(ws: WebSocket) {
+    wss.handleUpgrade(request, socket, head, function done(ws: WebSocketAndReq) {
+        ws.req = request;
         wss.emit("connection", ws, request);
     });
 }
